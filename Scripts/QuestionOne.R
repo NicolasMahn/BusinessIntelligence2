f153c6097433dee850bb22838c9c40c4282daaff
1Zero64 <1Zero64@users.noreply.github.com>
2021-12-15 20:18:03 +0100

    First Approach for Q1

---
+++ Scripts/QuestionOne.R
@@ -0,0 +1,18 @@
+#
+# First Approach
+#
+
+# Sort the data frames
+sortedTestData <- testData[order(testData$Qualitaet), ]
+sortedTrainData <- trainData[order(trainData$Qualitaet), ]
+
+model <- lm(Qualitaet~Durchmesser+Hoehe+Gewicht, sortedTrainData)
+prediction <- predict(model, sortedTestData)
+
+error <- prediction - sortedTestData$Qualitaet
+mean(abs(error))
+sqrt(mean(abs(error)))
+
+# Visualization
+plot(x=sortedTestData$Qualitaet, y=prediction)
+points(seq(1.0, 2.0), col="red", type = "l")
