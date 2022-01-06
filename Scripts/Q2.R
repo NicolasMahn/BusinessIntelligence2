fc972947dbbbe06a55853329855c03f36e7cb9cf
ArturErfurt <65763534+ArturErfurt@users.noreply.github.com>
2022-01-06 14:58:21 +0100

    Made Question 2

---
+++ Scripts/Q2.R
@@ -0,0 +1,8 @@
+Aufgabe 2:
+Erläutern Sie die von Ihnen zur Datenanalyse genutzten Konzepte
+
+Evaluieren und prüfen Sie Ihre Ergebnisse
+
+Prüfen Sie, auf welcher Basis Ihre Modelle die Prognose erzeugen
+
+Anmerkung: Ggf. können Sie auch zeigen, dass keine gute Vorhersage (mit den von uns behandelten Modellen)möglich ist.
