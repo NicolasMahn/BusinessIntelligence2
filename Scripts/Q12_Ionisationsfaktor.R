c767d8acf2bfebad7e01e95f0febf4af129d13dc
NicolasMahn <Nicolas.Mahn@gmx.de>
2022-01-23 13:53:55 +0100

    changed some minot things

--- Scripts/Q12_Ionisationsfaktor.R
+++ Scripts/Q12_Ionisationsfaktor.R
@@ -47,17 +47,17 @@ q12 <- function() {
 
 # Visualization
 q12Result <- function(){
-  plot(x=ionisationsfaktor, xlab="Ionisationsfaktor", xlim= c(10,40), 
+  plot(x=ionisationsfaktor, xlab="Ionisationsfaktor", xlim= c(12,35), 
        y=predictedIonisationsfaktor, ylab="Vorhergesagter Ionisationsfaktor", 
-       ylim= c(10,40), col=turquoise)
-  points(seq(1.0, 40.0), col=pink, type="l")
+       ylim= c(12,35), col=turquoise)
+  points(seq(1.0, 35.0), col=orange, type="l")
 }
 
 q12Errors <- function(){
   plot(x=seq(1,length(error),by=1),y=sortedError,
        xlab="sortierte Abweichungen",
        ylab="absoluter Abweichungswert",
-       col=pink)
+       col=orange)
   print(paste("Durchschnittliche Abweichung:", mean(sortedError)))
   #sqrt(mean(error))
 }
@@ -66,19 +66,19 @@ q12ErrorsNachGewicht <- function(){
   plot(x=seq(1,length(errorByWeight),by=1),y=errorByWeight,
        xlab="sortierte Abweichungen nach Gewicht",
        ylab="absoluter Abweichungswert",
-       col=pink)
+       col=orange)
 }
 
 q12ErrorsNachDurchmesser <- function(){
   plot(x=seq(1,length(errorByDiameter),by=1),y=errorByDiameter,
        xlab="sortierte Abweichungen nach Durchmesser",
        ylab="absoluter Abweichungswert",
-       col=pink)
+       col=orange)
 }
 
 q12ErrorsNachHoehe <- function(){
   plot(x=seq(1,length(errorByHeight),by=1),y=errorByHeight,
        xlab="sortierte Abweichungen nach H\U00F6he",
        ylab="absoluter Abweichungswert",
-       col=pink)
+       col=orange)
 }
